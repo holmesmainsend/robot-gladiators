c1492c785d8aed2cc530a649da34955b87397e5f
Mona Lisa <holmesmainsend@gmail.com>
2021-10-03 13:03:08 -0400

    added console features

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+var playerName = window.prompt("What is your robot's name?");
+  // Note the lack of quotation marks around playerName
+
+console.log(playerName);
+
+console.log("This log's a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+
+ // fight();
