a671ceb9135488580c2a924c78c9be81783d664e
Mona Lisa <holmesmainsend@gmail.com>
2021-10-04 07:27:37 -0400

    fixing loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -94,3 +94,32 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+
+// function to start a new game
+var startGame = function() {
+   // reset player stats
+   playerHealth = 100;
+   playerAttack = 10;
+   playerMoney = 10;
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+  }
+  // play again
+  startGame();
+};
+
+startGame();
+
+fight();
